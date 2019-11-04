3876be2458c84751b3ce7acde0f1a2fbd920307e
Vincent-Férié <arshey@live.fr>
2019-11-04 00:24:12 +0000

    update Model.php remove first try catch

--- app/Model.php
+++ app/Model.php
@@ -18,12 +18,6 @@ abstract class Model{
         $this->_connexion = null; // on efface s'il y'en a une autre connexion
 
         try {
-    $bdd = new PDO('mysql:host=localhost;dbname=licence2;charset=UTF8', 'root', 'root');
-} catch (Exception $e) {
-    die("Erreur : ".$e->getMessage);
-}
-
-        try {
             $this->_connexion = new PDO ('mysql:host='.$this->host.';dbname='.$this->db_name, $this->username, $this->password);
             $this->_connexion->exec('set names utf8');
         } catch (PDOException $e) {
